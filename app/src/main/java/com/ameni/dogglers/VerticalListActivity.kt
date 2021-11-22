ebc757d68bee832faeb342e9c67a4490b677c909
Ameni GDOURA <amenigdoura81@gmail.com>
2021-11-22 13:21:40 +0100

    Initial commit

---
+++ app/src/main/java/com/ameni/dogglers/VerticalListActivity.kt
@@ -0,0 +1,29 @@
+package com.ameni.dogglers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ameni.dogglers.adapter.DogCardAdapter
+import com.ameni.dogglers.const.Layout
+import com.ameni.dogglers.databinding.ActivityVerticalListBinding
+
+class VerticalListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVerticalListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerticalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.verticalRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.VERTICAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.verticalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
